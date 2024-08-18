27b57ea8f89ec7da9da5684e8dbd8a700a1b4973
Iridium <moisxphillip@gmail.com>
2024-08-18 17:16:45 -0300

    started prototyping menu, some tweaks

--- scenes/scenario.gd
+++ scenes/scenario.gd
@@ -1,10 +1,15 @@
 extends Node3D
-
-@onready var Player = get_tree().current_scene.get_node("Player")
-@onready var Application = get_tree().current_scene.get_node("Application")
+#
+#@onready var Player = get_tree().current_scene.get_node("Player")
+#@onready var Application = get_tree().current_scene.get_node("Application")
 
 func _ready():
+    #init_stage()
     pass # Replace with function body.
 
+#func init_stage():
+    #Player = get_tree().current_scene.get_node("Player")
+    #Application = get_tree().current_scene.get_node("Application")
+    
 func _process(_delta):
     pass
