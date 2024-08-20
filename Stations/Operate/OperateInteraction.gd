d000c9f0851052499060ec0b2df2d695e3c8ff81
MoustacheGolem <mateus_p_r@hotmail.com>
2024-08-20 11:49:14 -0300

    artgamer

--- Stations/Operate/OperateInteraction.gd
+++ Stations/Operate/OperateInteraction.gd
@@ -5,21 +5,22 @@ var isClose = false
 var playerReference = null
 
 func _ready():
-    animation.play("Off")
+	animation.play("Off")
+	$AnimatedSprite3D.play("default")
 
 func _process(_delta):
-    if Input.is_action_just_pressed("Interact") and isClose:
-        $"../../Store".show()
-        playerReference.drop_item()
-        playerReference.isLockedOnAction = true
+	if Input.is_action_just_pressed("Interact") and isClose:
+		$"../../Store".show()
+		playerReference.drop_item()
+		playerReference.isLockedOnAction = true
 
 func _on_body_entered(_body):
-    if _body.is_in_group("player") and _body.isHoldingApplication:
-        animation.play("On")
-        isClose = true
-        playerReference = _body
+	if _body.is_in_group("player") and _body.isHoldingApplication:
+		animation.play("On")
+		isClose = true
+		playerReference = _body
 
 func _on_body_exited(_body):
-    isClose = false
-    playerReference = null
-    animation.play("Off")
+	isClose = false
+	playerReference = null
+	animation.play("Off")
