894b692d74fc7f4e6aceb0b22ed6a8b41e2c816b
Iridium <moisxphillip@gmail.com>
2024-08-20 10:19:20 -0300

    tests, changes...

--- Interaction.gd
+++ Interaction.gd
@@ -5,29 +5,29 @@ var isClose = false
 var playerReference = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation.play("Off")
+    animation.play("Off")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	if Input.is_action_just_pressed("Interact") and isClose:
-		$Display.show()
-		$SubViewport/TestStationUI.IsInteracting = true
-		$SubViewport/TestStationUI.applicationReference = playerReference.applicationReference
-		$SubViewport/TestStationUI.playerReference = playerReference
-		playerReference.drop_item()
-		playerReference.isLockedOnAction = true
+    if Input.is_action_just_pressed("Interact") and isClose:
+        $Display.show()
+        $SubViewport/TestStationUI.IsInteracting = true
+        $SubViewport/TestStationUI.applicationReference = playerReference.applicationReference
+        $SubViewport/TestStationUI.playerReference = playerReference
+        playerReference.drop_item()
+        playerReference.isLockedOnAction = true
 
 
 func _on_body_entered(_body):
-	if _body.is_in_group("player") and _body.isHoldingApplication:
-		#if not $SubViewport/TestStationUI.IsInteracting:
-			#animation.play("On")
-		isClose = true
-		playerReference = _body
-	
+    if _body.is_in_group("player") and _body.isHoldingApplication:
+        #if not $SubViewport/TestStationUI.IsInteracting:
+            #animation.play("On")
+        isClose = true
+        playerReference = _body
+    
 
 
 func _on_body_exited(_body):
-	isClose = false
-	playerReference = null
-	animation.play("Off")
+    isClose = false
+    playerReference = null
+    animation.play("Off")
